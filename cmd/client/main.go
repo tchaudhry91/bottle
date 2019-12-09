3132ef9ebcab8b1391cc2cfbd1de6804642ffdb5
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2019-12-09 18:01:35 +0000

    Refactor bottle cli

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,86 +2,132 @@ package main
 
 import (
 	"context"
-	"flag"
 	"fmt"
-	"github.com/peterbourgon/ff"
 	"github.com/tchaudhry91/bottle/bottle"
 	"github.com/tchaudhry91/bottle/crate"
 	"github.com/tchaudhry91/bottle/crate/pb"
+	"github.com/urfave/cli/v2"
 	"google.golang.org/grpc"
+	"log"
 	"os"
+	"strings"
 )
 
 func main() {
-	fs := flag.NewFlagSet("bottle", flag.ExitOnError)
-	var (
-		serverAddr = fs.String("server-addr", "127.0.0.1:14999", "crate service to connect to")
-		id         = fs.String("id", "", "bottle id")
-		mode       = fs.String("mode", "store", "Mode to operate under:drain/pour/store")
-	)
-	err := ff.Parse(fs, os.Args[1:], ff.WithEnvVarNoPrefix())
+	var serverAddr string
+	var mode string
+	var id string
+
+	app := &cli.App{
+		Name:  "bottle",
+		Usage: "Store data from pipes",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "server",
+				Aliases:     []string{"s"},
+				Value:       "",
+				EnvVars:     []string{"BOTTLE_SERVER"},
+				Usage:       "Address of the remote server",
+				Destination: &serverAddr,
+			},
+			&cli.StringFlag{
+				Name:        "mode",
+				Aliases:     []string{"m"},
+				Value:       "fill",
+				Usage:       "Mode to operate in, fill/drain/pour",
+				EnvVars:     []string{"BOTTLE_MODE"},
+				Destination: &mode,
+			},
+			&cli.StringFlag{
+				Name:        "id",
+				Aliases:     []string{"i"},
+				Value:       "clip",
+				EnvVars:     []string{"BOTTLE_ID"},
+				Usage:       "ID of the bottle to operate on",
+				Destination: &id,
+			},
+		},
+		Action: func(c *cli.Context) error {
+			conn, err := grpc.Dial(serverAddr, grpc.WithInsecure())
+			if err != nil {
+				panic(err)
+			}
+			// Build Client
+			client := pb.NewCrateClient(conn)
+
+			switch strings.ToLower(mode) {
+			case "fill":
+				return fill(id, client)
+			case "pour":
+				return pour(id, client)
+			case "drain":
+				return drain(id, client)
+			}
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
+}
 
-	conn, err := grpc.Dial(*serverAddr, grpc.WithInsecure())
+func fill(id string, client pb.CrateClient) error {
+	b := bottle.NewBottle(id)
+	err := b.Fill(os.Stdin)
 	if err != nil {
-		panic(err)
+		fmt.Printf("Failed to store bottle: %s", err)
+		return err
 	}
-
-	// Build Client
-	client := pb.NewCrateClient(conn)
-
-	switch *mode {
-	case "store":
-		b := bottle.NewBottle(*id)
-		err = b.Fill(os.Stdin)
-		if err != nil {
-			fmt.Printf("Failed to store bottle: %s", err)
-			os.Exit(1)
-		}
-		resp, err := client.Store(context.Background(), &pb.StoreRequest{Bottle: crate.BottleToPB(b)})
-		if err != nil {
-			fmt.Printf("Failed to store bottle: %s", err)
-			os.Exit(1)
-		}
-		if resp.Err != "" {
-			fmt.Printf("Failed to store bottle: %s", resp.Err)
-			os.Exit(1)
-		}
-	case "pour":
-		var b *bottle.Bottle
-		resp, err := client.Pour(context.Background(), &pb.GetRequest{Id: *id})
-		if err != nil {
-			fmt.Printf("Failed to fetch bottle: %s", err)
-			os.Exit(1)
-		}
-		if resp.Err != "" {
-			fmt.Printf("Failed to fetch bottle: %s", resp.Err)
-			os.Exit(1)
-		}
-		b = crate.PBToBottle(resp.Bottle)
-		err = b.Pour(os.Stdout)
-		if err != nil {
-			fmt.Printf("Failed to pour bottle: %s", err)
-			os.Exit(1)
-		}
-	case "drain":
-		var b *bottle.Bottle
-		resp, err := client.Drain(context.Background(), &pb.GetRequest{Id: *id})
-		if err != nil {
-			fmt.Printf("Failed to fetch bottle to drain: %s", err)
-			os.Exit(1)
-		}
-		if resp.Err != "" {
-			fmt.Printf("Failed to fetch bottle to drain: %s", resp.Err)
-			os.Exit(1)
-		}
-		b = crate.PBToBottle(resp.Bottle)
-		err = b.Drain(os.Stdout)
-		if err != nil {
-			fmt.Printf("Failed to drain bottle: %s", err)
-			os.Exit(1)
-		}
+	resp, err := client.Store(context.Background(), &pb.StoreRequest{Bottle: crate.BottleToPB(b)})
+	if err != nil {
+		fmt.Printf("Failed to store bottle: %s", err)
+		return err
 	}
+	if resp.Err != "" {
+		fmt.Printf("Failed to store bottle: %s", resp.Err)
+		return fmt.Errorf(resp.Err)
+	}
+	return nil
+}
+
+func pour(id string, client pb.CrateClient) error {
+	var b *bottle.Bottle
+	resp, err := client.Pour(context.Background(), &pb.GetRequest{Id: id})
+	if err != nil {
+		fmt.Printf("Failed to fetch bottle: %s", err)
+		return err
+	}
+	if resp.Err != "" {
+		fmt.Printf("Failed to fetch bottle: %s", resp.Err)
+		return fmt.Errorf(resp.Err)
+	}
+	b = crate.PBToBottle(resp.Bottle)
+	err = b.Pour(os.Stdout)
+	if err != nil {
+		fmt.Printf("Failed to pour bottle: %s", err)
+		return err
+	}
+	return nil
+}
+
+func drain(id string, client pb.CrateClient) error {
+	var b *bottle.Bottle
+	resp, err := client.Drain(context.Background(), &pb.GetRequest{Id: id})
+	if err != nil {
+		fmt.Printf("Failed to fetch bottle to drain: %s", err)
+		return err
+	}
+	if resp.Err != "" {
+		fmt.Printf("Failed to fetch bottle to drain: %s", resp.Err)
+		return err
+	}
+	b = crate.PBToBottle(resp.Bottle)
+	err = b.Drain(os.Stdout)
+	if err != nil {
+		fmt.Printf("Failed to drain bottle: %s", err)
+		return err
+	}
+	return nil
 }
--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/golang/protobuf v1.3.2
 	github.com/peterbourgon/ff v1.6.0
+	github.com/urfave/cli/v2 v2.0.0
 	golang.org/x/lint v0.0.0-20190313153728-d0100b6bd8b3 // indirect
 	golang.org/x/tools v0.0.0-20190524140312-2c0ae7006135 // indirect
 	google.golang.org/grpc v1.25.1
